88b2bfb6168a17acf585dcf4c683d27d594183b5
Javier Alvarez <javier@crowsoft.com.ar>
2020-12-08 17:16:54 -0300

    js to ts

--- js/CSReport/CSReportScript/Codigo/CSReportScript/cIReportScriptType.js
+++
@@ -1,9 +0,0 @@
-(function(globalObject) {
-
-    globalObject.CSReportScript = globalObject.CSReportScript || {}; //@@@: namespace CSReportScript
- //@@@: {
-UNKNOWN >>     public interface cIReportScriptType //@@@: public interface cIReportScriptType
-    { //@@@: {
-        string RunScript(cReportCompilerGlobals globals); //@@@: string RunScript(cReportCompilerGlobals globals);
-    } //@@@: }
-} //@@@: }
